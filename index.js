d592f11c042f192ac969138d0da8ccaac510ec24
mengaww2ya <mengaww2ya@gmail.com>
2024-05-19 23:31:58 +0300

    un finished hamburger

---
+++ index.js
@@ -0,0 +1,4 @@
+const button=document.getElementsByClassName('.buton')[0];
+const option-document.getElementsByClassName('.optionbtn')[0];
+
+button.addEventListener('click',()=>{option.classList.toggle('active')});
