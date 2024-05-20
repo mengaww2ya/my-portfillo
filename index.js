c81c3f64f8f1ecef660793b1d862e0392558e759
mengaww2ya <mengaww2ya@gmail.com>
2024-05-20 21:08:35 +0300

    updated hamburger

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 function displayOption() {
+  const close = document.querySelector('.xx');
+  const button = document.querySelector('.buton');
   const option = document.querySelector('.optionbtn');
   option.style.display = 'flex';
+  button.style.display = 'none';
+  close.style.display = 'block';
 }
 function closeOption() {
+  const close = document.querySelector('.xx');
+  const button = document.querySelector('.buton');
   const option = document.querySelector('.optionbtn');
   option.style.display = 'none';
+  button.style.display = 'flex';
+  close.style.display = 'none';
+}
+
+function closex() {
+  const close = document.querySelector('.xx');
+  const button = document.querySelector('.buton');
+  const option = document.querySelector('.optionbtn');
+  option.style.display = 'none';
+  close.style.display = 'none';
+  button.style.display = 'flex';
 }
 displayOption();
 closeOption();
+closex();
